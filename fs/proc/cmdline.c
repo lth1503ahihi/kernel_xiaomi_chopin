4279612b0b08236da20b482a7fb203961769e19e
lth1503ahihi <nguyenvictor2000o@gmail.com>
2026-08-31 14:13:50 -0700

    hi

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -6,25 +6,26 @@
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", saved_command_line);
-	return 0;
+ /* In nguyên vẹn cmdline gốc, sau đó ghi đè cờ locked/green vào đuôi */
+ seq_printf(m, "%s androidboot.verifiedbootstate=green androidboot.flash.locked=1 androidboot.vbmeta.device_state=locked\n", saved_command_line);
+ return 0;
 }
 
 static int cmdline_proc_open(struct inode *inode, struct file *file)
 {
-	return single_open(file, cmdline_proc_show, NULL);
+ return single_open(file, cmdline_proc_show, NULL);
 }
 
 static const struct file_operations cmdline_proc_fops = {
-	.open		= cmdline_proc_open,
-	.read		= seq_read,
-	.llseek		= seq_lseek,
-	.release	= single_release,
+ .open  = cmdline_proc_open,
+ .read  = seq_read,
+ .llseek  = seq_lseek,
+ .release = single_release,
 };
 
 static int __init proc_cmdline_init(void)
 {
-	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
-	return 0;
+ proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
+ return 0;
 }
 fs_initcall(proc_cmdline_init);
